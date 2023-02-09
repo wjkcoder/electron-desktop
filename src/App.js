6d3301794af32f0f0cec0795c788b4365fc61eca
wonder <wonder@charsmart.com>
2023-02-09 13:31:10 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UploadPic from './components/upload';
+
+function App() {
+  return (
+    <div className="App">
+      <UploadPic/>
+    </div>
+  );
+}
+
+export default App;
